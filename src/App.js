d62891cbcd1fdaddef0a664acc954dde44b19ae2
Preet Patel <145058871+00preetpatel@users.noreply.github.com>
2023-12-16 21:32:34 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import VideoBackground from './components/VideoBackground';
+import MenuBar from './components/MenuBar';
+import ContentContainer from './components/ContentContainer';
+import Profile from './components/Profile';
+import Projects from './components/Projects';
+import Courses from './components/Courses';
 
 function App() {
+  const [selectedMenu, setSelectedMenu] = useState('');
+
+  const renderContent = () => {
+    switch (selectedMenu) {
+      case 'profile':
+        return <Profile />; 
+      case 'projects':
+        return <Projects />; 
+      case 'courses':
+        return <Courses />; 
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VideoBackground />
+      <MenuBar onMenuSelect={setSelectedMenu} />
+      <ContentContainer selectedMenu={selectedMenu}>
+        {renderContent()}
+      </ContentContainer>
     </div>
   );
 }
