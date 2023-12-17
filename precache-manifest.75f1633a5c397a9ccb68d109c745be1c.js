ec8586e8ba5aefb9375824ed4e802d85684e7288
Preet Patel <145058871+00preetpatel@users.noreply.github.com>
2023-12-16 21:43:41 -0500

    Updates

---
+++ precache-manifest.75f1633a5c397a9ccb68d109c745be1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5cbd0a82ea6838b5cf3c40d96bd314",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cc77ba39b7a04fbe69e8",
+    "url": "/portfolio/static/css/main.13fd667a.chunk.css"
+  },
+  {
+    "revision": "b5fe5a82b6f349815fe3",
+    "url": "/portfolio/static/js/2.7067bb89.chunk.js"
+  },
+  {
+    "revision": "cc77ba39b7a04fbe69e8",
+    "url": "/portfolio/static/js/main.72ef2f62.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "66694fb8db6694d91b938d3bacc8aa1d",
+    "url": "/portfolio/static/media/BackgroundAudio.66694fb8.mp3"
+  },
+  {
+    "revision": "3176a76c4234f46378282a0af26aebf1",
+    "url": "/portfolio/static/media/BackgroundVideo.3176a76c.mp4"
+  },
+  {
+    "revision": "64837867854a1661b2f68abf97883475",
+    "url": "/portfolio/static/media/ProfilePicture.64837867.jpeg"
+  }
+]);
